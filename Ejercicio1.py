bdb8801bc0b1fa43b552ee7928932f40ca1e284c
blancabalmori <91721863+blancabalmori@users.noreply.github.com>
2021-12-14 10:48:09 +0100

    Me trabé :(

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,5 +1,11 @@
 def minion_game (cadena):
     puntuacionstuart = 0
     puntuacionkevin = 0
+    vocal = 'aeiouAEIOU'
     cadena = input("Escoja una palabra: ")
     for i in range (len(cadena)):
+        if cadena :#empezar en consonante
+            puntuacionstuart +=
+        else: #empezar en vocal
+            puntuacionkevin +=
+
