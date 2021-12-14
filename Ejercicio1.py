94748a03343fccd901f60147170b1cfada46a08e
blancabalmori <91721863+blancabalmori@users.noreply.github.com>
2021-12-14 11:04:53 +0100

    JUJUUJU creo q va

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,8 +4,8 @@ def minion_game (cadena):
     vocal = 'aeiouAEIOU'
     cadena = input("Escoja una palabra: ")
     for i in range (len(cadena)):
-        if cadena :#empezar en consonante
-            puntuacionstuart +=
-        else: #empezar en vocal
-            puntuacionkevin +=
-
+        if cadena[i] not in vocal:
+            puntuacionstuart += (len(cadena))
+        else: 
+            puntuacionkevin += (len(cadena))
+    print ("La puntuación de Stuart es " + puntuacionstuart + "y la puntuación de Kevin es " + puntuacionkevin)
